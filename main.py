0281c84070008f1c2555035f0701ea9802f0c0f9
Ethan W <weberet@gmail.com>
2021-04-01 16:45:25 -0600

    model cleaning, make prediction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 import modules.model as myModel
+import pandas
 from sklearn.linear_model import LinearRegression
 from flask import Flask, render_template
 from flask import templating
@@ -8,7 +9,7 @@ app = Flask(__name__)
 #Global model creation for start of application
 data = myModel.get_dataset()
 #region Menu Options
-manufacturerOptions = myModel.get_unique_values(data,'manufacturer').tolist()
+manufacturerOptions = sorted(myModel.get_unique_values(data,'manufacturer').tolist())
 print(f'Manufacturer options:\n {manufacturerOptions}')
 
 #yearOptions population
@@ -17,14 +18,28 @@ today = date.today()
 for i in range(5,25):
     yearOptions.append(today.year - i)
 
-conditionOptions = myModel.get_unique_values(data,'condition').tolist()
+#conditionOptions = myModel.get_unique_values(data,'condition').tolist()
+conditionOptions = ['salvage','fair','good','excellent','like new', 'new']
+
 titleOptions = myModel.get_unique_values(data,'title_status').tolist()
-cylinderOptions = myModel.get_unique_values(data,'cylinders').tolist()
+print(titleOptions)
+#cylinderOptions = myModel.get_unique_values(data,'cylinders').tolist()
+print(f'Unique cylinder options = {myModel.get_unique_values(data,"cylinders").tolist()}')
+cylinderOptions = [3,4,5,6,8]
 fuelOptions = myModel.get_unique_values(data,'fuel').tolist()
 #endregion
-#X = myModel.get_X(data)
-#y = myModel.get_y(data)
-#trainedModel = myModel.get_trained_model(X,y)
+X = myModel.get_X(data)
+y = myModel.get_y(data)
+trainedModel = myModel.get_trained_model(X,y)
+rootMeanError = myModel.get_mean_error(trainedModel,myModel.get_train_split_var(X,y,'X_Test'),
+                             myModel.get_train_split_var(X,y,'y_test'))
+standardScaler = myModel.get_StandardScaler(data)
+
+
+
+#singlePrediction = trainedModel.predict(['acura',4,8,].reshape(1,-1))
+#print(f'Single prediction {singlePrediction}')
+print(f'Root Mean Error for trained model = {rootMeanError}')
 
 @app.route('/')
 def index():
@@ -34,8 +49,7 @@ def index():
 
 @app.route('/predict')
 def predict():
-    return 'Prediction route'
-    #return trainedModel.predict([vars])
+    return trainedModel.predict([vars])
 
 @app.route('/graphs')
 def graphs():
@@ -45,7 +59,64 @@ def graphs():
 def stats():
     return 'Stats page'
 
+def makePrediction(manufacturer,year,condition,title,cylinder,fuel,mileage):
+    predDictionary = {
+        'condition': [0],
+        'cylinders': [0],
+        'odometer': [0],
+        'age': [0],
+        'title_clean': [0],
+        'title_lien': [0],
+        'title_rebuilt': [0],
+        'manu_acura': [0],
+        'manu_audi': [0],
+        'manu_bmw': [0],
+        'manu_buick': [0],
+        'manu_cadillac': [0],
+        'manu_chevrolet': [0],
+        'manu_chrysler': [0],
+        'manu_dodge': [0],
+        'manu_fiat': [0],
+        'manu_ford': [0],
+        'manu_gmc': [0],
+        'manu_harley-davidson': [0],
+        'manu_honda': [0],
+        'manu_hyundai': [0],
+        'manu_infiniti': [0],
+        'manu_jaguar': [0],
+        'manu_jeep': [0],
+        'manu_kia': [0],
+        'manu_land rover': [0],
+        'manu_lexus': [0],
+        'manu_lincoln': [0],
+        'manu_mazda': [0],
+        'manu_mercedes-benz': [0],
+        'manu_mercury': [0],
+        'manu_mini': [0],
+        'manu_mitsubishi': [0],
+        'manu_nissan': [0],
+        'manu_pontiac': [0],
+        'manu_porsche': [0],
+        'manu_ram': [0],
+        'manu_rover': [0],
+        'manu_saturn': [0],
+        'manu_subaru': [0],
+        'manu_toyota': [0],
+        'manu_volkswagen': [0],
+        'manu_volvo': [0],
+        'fuel_diesel': [0],
+        'fuel_electric': [0],
+        'fuel_gas': [0],
+        'fuel_hybrid': [0],
+        'fuel_other': [0],
+    }
 
+    predDF = pandas.DataFrame.from_dict(predDictionary)
+    print(f'predDataFrame = \n{predDF}')
+    aPred = trainedModel.predict(standardScaler.transform(predDF))
+    print(f'A Prediction = {aPred[0][0]}')
+
+makePrediction('acura',2016,'salvage','lien',4,'diesel',120000)
 
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
--- modules/model.py
+++ modules/model.py
@@ -23,7 +23,7 @@ def get_dataset():
     datafile = filedirectory+os.sep+filename
 
     #New code using only optimized columns
-    vdata = pandas.read_csv(datafile,usecols=['price','year','manufacturer','model','condition','odometer','title_status', 'cylinders','fuel'])
+    vdata = pandas.read_csv(datafile,usecols=['price','year','manufacturer','condition','odometer','title_status', 'cylinders','fuel'])
     #Columns used: price, age, manufacturer, model, condition, odometer, title_status, cylinders, fuel
 
     print(vdata.dtypes)
@@ -33,11 +33,11 @@ def get_dataset():
     #vdata['manufacturer'] = LabelEncoder().fit_transform(vdata['manufacturer'])
 
     # vdata = onehot_encode(vdata,['model'],['model'])
-    vdata['model'] = vdata['model'].astype('category')
-    vdata['model'] = LabelEncoder().fit_transform(vdata['model'])
+    #vdata['model'] = vdata['model'].astype('category')
+    #vdata['model'] = LabelEncoder().fit_transform(vdata['model'])
 
     vdata['condition'] = vdata['condition'].astype('category')
-    vdata['condition'] = LabelEncoder().fit_transform(vdata['condition'])
+    #vdata['condition'] = LabelEncoder().fit_transform(vdata['condition'])
 
     #Changing feature of year into "age" for better model
     vdata['age'] = [date.today().year - i for i in vdata['year'].tolist()]
@@ -50,14 +50,16 @@ def get_dataset():
     vdata = vdata.dropna()
 
     #Keep only clean or better cars to remove outliers
-    #vdata = vdata[((vdata['title_status'] != 'missing') & (vdata['title_status'] != 'parts only')
-    #    & (vdata['title_status'] != 'salvage') & (vdata['title_status'] != 'rebuilt')
-    #   & (vdata['title_status'] != 'lien'))]
+    vdata = vdata[((vdata['title_status'] != 'missing') & (vdata['title_status'] != 'parts only') & (vdata['title_status'] != 'salvage'))]
+    # & (vdata['title_status'] != 'rebuilt') & (vdata['title_status'] != 'lien')
 
     #Condition to "rating":
-    #vdata['condition'].replace('new', 6, inplace = True)
-    #vdata['condition'].replace('like new', 5, inplace = True)
-    #vdata['condition'].replace('excellent', 4, inplace = True)
+    vdata['condition'].replace('new', 6, inplace = True)
+    vdata['condition'].replace('like new', 5, inplace = True)
+    vdata['condition'].replace('excellent', 4, inplace = True)
+    vdata['condition'].replace('good', 3, inplace = True)
+    vdata['condition'].replace('fair', 2, inplace = True)
+    vdata['condition'].replace('salvage', 1, inplace = True)
 
     #remove Some Outliers pricewise:
     vdata['price'] = vdata[vdata['price'] < 99999]
@@ -75,14 +77,17 @@ def get_dataset():
     #Cylinders to ints
     vdata['cylinders'] = vdata['cylinders'].replace('other', numpy.NaN)
     vdata['cylinders'] = vdata['cylinders'].str.extract('(\d+)', expand=False)
+
     #Drop NaN values
     vdata = vdata.dropna()
 
     vdata['cylinders'] = vdata['cylinders'].astype(int)
-    print('Got here again')
+    #vdata = vdata[vdata.cylinders != 3]
+    vdata = vdata[vdata.cylinders != 10]
+    vdata = vdata[vdata.cylinders != 12]
 
-    #vdata['cylinders'] = vdata[vdata['cylinders'] > 3]
-    #vdata['cylinders'] = vdata[vdata['cylinders'] < 9]
+    #Drop unfit / exotic brands:
+    #vdata = vdata[vdata.manufacturer != 'harley-davidson']
 
     ##Onehot encode necessary stuff
     vdata = onehot_encode(vdata,['title_status','manufacturer','fuel'],['title','manu','fuel'],False)
@@ -96,10 +101,19 @@ def get_dataset():
 def get_X(dataset):
     X = dataset.drop(['price','title_status','manufacturer','fuel'], axis=1) #Skip these columns as not used or using onehot encoding.
 
-    scaler = StandardScaler()
+    scaler = get_StandardScaler(dataset)
+    print(f'X = {X}')
 
     X = scaler.fit_transform(X)
     return X
+
+def get_StandardScaler(dataset):
+    X = dataset.drop(['price','title_status','manufacturer','fuel'], axis=1) #Skip these columns as not used or using onehot encoding.
+
+    scaler = StandardScaler()
+
+    scaler.fit_transform(X)
+    return scaler
 
 def get_y(dataset):
     y = dataset[['price']] #column predictions target
@@ -134,7 +148,7 @@ def get_r2(model,X_Test,y_test):
     prediction = model.predict(X_Test)
 
     r2score = r2_score(y_test,prediction)
-    print(f'r2score = {r2score}')
+    #print(f'r2score = {r2score}')
     return r2score
 
 def get_mean_error(model,X_Test,y_test):
@@ -142,7 +156,7 @@ def get_mean_error(model,X_Test,y_test):
 
     #Determine mean absoloute error
     meanError = mean_absolute_error(y_test,prediction)
-    print(f'Mean Absoloute Error = {meanError}')
+    #print(f'Mean Absoloute Error = {meanError}')
     return meanError
 
 def get_rmse(model, X_Test,y_test):
@@ -150,7 +164,7 @@ def get_rmse(model, X_Test,y_test):
     #Root Mean Squared Error:
     rMeanSquareE = mean_squared_error(y_test,prediction)
     rMeanSquareE = numpy.sqrt(rMeanSquareE)
-    print(f'Mean Square Error = {rMeanSquareE}')
+    #print(f'Mean Square Error = {rMeanSquareE}')
     return rMeanSquareE
 
 def get_unique_values(dataset,column):
